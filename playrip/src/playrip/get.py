381abe52c9c49b1b3f2ecb7c7b6dd802e7dc39c9
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2026-03-03 20:20:44 -0300

    Best resolution and audio in mp4 dowload

--- playrip/src/playrip/get.py
+++ playrip/src/playrip/get.py
@@ -6,6 +6,7 @@ import re
 import os
 from pydub import AudioSegment
 from pytubefix import Search, YouTube
+import subprocess
 import logging
 
 logging.getLogger("spotipy").setLevel(logging.CRITICAL)
@@ -14,14 +15,14 @@ client_secret = "eb3c7e469f40400b941dc05116cfc55b"
 
 
 def id(url):
-    if("youtu" in url):
+    if "youtu" in url:
         patterns = [r"(?:youtu\.be/)([^?&]+)", r"(?:v=)([^?&]+)"]
         for pattern in patterns:
             match = re.search(pattern, url)
             if match:
                 return match.group(1)
         return None
-    elif("spotify" in url):
+    elif "spotify" in url:
         track_id = url.split("/track/")[1].split("?")[0]
         return track_id
     else:
@@ -95,7 +96,10 @@ def audio(url, diretorio_destino):
                 .replace(":", "")
                 .replace("\\", "")
             )
-            os.rename(f"{diretorio_destino}/{titulo_novo1}.m4a", f"{diretorio_destino}/{Novo_titulo_spotify}.m4a")
+            os.rename(
+                f"{diretorio_destino}/{titulo_novo1}.m4a",
+                f"{diretorio_destino}/{Novo_titulo_spotify}.m4a",
+            )
             sound = AudioSegment.from_file(
                 f"{diretorio_destino}/{Novo_titulo_spotify}.m4a", format="m4a"
             )
@@ -127,4 +131,47 @@ def audio(url, diretorio_destino):
 
 def video(url, diretorio_destino):
     yt = YouTube(url)
-    yt.streams.get_highest_resolution().download(output_path=diretorio_destino)
+    titulo = yt.title
+    titulo_novo1 = (
+        titulo.replace("/", "")
+        .replace("|", "")
+        .replace("?", "")
+        .replace("*", "")
+        .replace("<", "")
+        .replace(">", "")
+        .replace(":", "")
+        .replace("\\", "")
+    )
+    video = (
+        yt.streams.filter(progressive=False, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+    )
+    audio = yt.streams.filter(only_audio=True).order_by("abr").desc().first()
+    video.download(filename="video.mp4", output_path=diretorio_destino)
+    audio.download(filename="audio.mp4", output_path=diretorio_destino)
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-hide_banner",
+            "-loglevel",
+            "error",
+            "-i",
+            f"{diretorio_destino}/video.mp4",
+            "-i",
+            f"{diretorio_destino}/audio.mp4",
+            "-map",
+            "0:v:0",
+            "-map",
+            "1:a:0",
+            "-c:v",
+            "copy",
+            "-c:a",
+            "aac",
+            "-shortest",
+            f"{diretorio_destino}/{titulo_novo1}.mp4",
+        ]
+    )
+    os.remove(f"{diretorio_destino}/audio.mp4")
+    os.remove(f"{diretorio_destino}/video.mp4")
